7fc98befba2c94f1eac143b2988050bb0c7365af
amature-build <98519612+amature-build@users.noreply.github.com>
2022-12-17 00:44:39 -0800

    Update streamlit_app.py
    Added emojis.
    Fixed type in line 7 from Kate to Kale

--- streamlit_app.py
+++ streamlit_app.py
@@ -3,7 +3,7 @@ import streamlit as st
 st.title('My Parents New Healthy Diner')
 
 st.header('Breakfast Menu')
-st.text('Omega 3 & Blueberry Oatmeal')
-st.text('Kate, Spinach & Rocket Smootie')
-st.text('Hard-Boiled Free-Range Egg')
-  
+st.text('🥣 Omega 3 & Blueberry Oatmeal')
+st.text('🥗 Kale, Spinach & Rocket Smootie')
+st.text('🐔 Hard-Boiled Free-Range Egg')
+st.text('🥑🍞 Avocado Toast')
