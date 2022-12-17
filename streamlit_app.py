02926fba010226d1fd853f340d14f480b2e1a40f
amature-build <98519612+amature-build@users.noreply.github.com>
2022-12-17 01:02:08 -0800

    Update streamlit_app.py
    added selection in fruit_selected for testing

--- streamlit_app.py
+++ streamlit_app.py
@@ -18,7 +18,7 @@ my_fruit_list = pd.read_csv(r"https://uni-lab-files.s3.us-west-2.amazonaws.com/d
 my_fruit_list = my_fruit_list.set_index('Fruit')
 
 # Let's put a pick list here so they can pick the fruit they want to include 
-fruit_selected = st.multiselect("Pick some fruits:", list(my_fruit_list.index))
+fruit_selected = st.multiselect("Pick some fruits:", list(my_fruit_list.index), ['Apple', 'Avocado'])
 
 fruit_to_show = my_fruit_list.loc[fruit_selected]
 
