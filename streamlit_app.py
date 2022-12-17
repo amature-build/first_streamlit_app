c4e5bef4d77aa3b64bb52484ebea759fe606ecf5
amature-build <98519612+amature-build@users.noreply.github.com>
2022-12-17 00:45:27 -0800

    Update streamlit_app.py
    Typo Smoothie

--- streamlit_app.py
+++ streamlit_app.py
@@ -4,6 +4,6 @@ st.title('My Parents New Healthy Diner')
 
 st.header('Breakfast Menu')
 st.text('🥣 Omega 3 & Blueberry Oatmeal')
-st.text('🥗 Kale, Spinach & Rocket Smootie')
+st.text('🥗 Kale, Spinach & Rocket Smoothie')
 st.text('🐔 Hard-Boiled Free-Range Egg')
 st.text('🥑🍞 Avocado Toast')
